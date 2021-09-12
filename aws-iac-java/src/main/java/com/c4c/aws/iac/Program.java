70121d2772c99d8b2a97ea3944bc9b367b59d27a
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2021-09-12 20:58:58 +0530

    Added CDK base iac project for creating vpc, subnets and EKS cluster

--- aws-iac-java/src/main/java/com/c4c/aws/iac/Program.java
+++ aws-iac-java/src/main/java/com/c4c/aws/iac/Program.java
@@ -1,5 +1,6 @@
 package com.c4c.aws.iac;
 
+import com.c4c.aws.iac.stacks.EksClusterStack;
 import com.c4c.aws.iac.stacks.RoleStack;
 import software.amazon.awscdk.core.App;
 
@@ -8,7 +9,7 @@ public class Program {
         System.out.println("Welcome to AWS CDK");
         App app = new App();
 
-        new RoleStack(app, "EKS-CLUSTER");
+        new EksClusterStack(app, "C4C-EKS-CLUSTER-STACK");
 
         // required until https://github.com/aws/jsii/issues/456 is resolved
         app.synth();
