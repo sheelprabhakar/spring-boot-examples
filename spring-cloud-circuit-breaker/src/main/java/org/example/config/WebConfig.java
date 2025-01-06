180041172f92a8dc8e5f3eca0f2853cc4ac9a5dc
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2025-01-06 11:02:39 +0530

    Spring boot username password example

---
+++ spring-cloud-circuit-breaker/src/main/java/org/example/config/WebConfig.java
@@ -0,0 +1,29 @@
+package org.example.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.circuitbreaker.springretry.SpringRetryCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+
+import java.time.Duration;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public Customizer<SpringRetryCircuitBreakerFactory> slowCustomizer() {
+        return factory -> factory.configure(builder -> builder.retryPolicy(new SimpleRetryPolicy(1)).build(), "slow");
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .build());
+    }
+}
---
+++ spring-security-basic/src/main/java/com/c4c/example/basic/rest/controller/SecurityController.java
@@ -0,0 +1,28 @@
+package com.c4c.example.basic.rest.controller;
+
+import com.c4c.example.basic.core.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class SecurityController {
+  @Autowired
+  UserService userService;
+  @GetMapping("/home")
+  public  String home(){
+    return "This is Home";
+  }
+  @GetMapping("/student")
+  public  String student(){
+    return "This is Student";
+  }
+  @GetMapping("/admin")
+  public  String admin(){
+    return "This is Admin";
+  }
+  @PostMapping("/create")
+  public String create(@RequestParam("username") String username, @RequestParam("password") String password){
+    return  userService.create(username, password);
+  }
+}
