d38eaed6427141c3335ee64cae92305da1f2adc7
Sheel Prabhakar <sheel.prabhakar@gmail.com>
2025-04-06 10:05:39 +0530

    Added multi tenant MYSQL example

---
+++ mysql-mtenant-test/src/main/java/org/c4c/mysql/service/UserService.java
@@ -0,0 +1,41 @@
+package org.c4c.mysql.service;
+
+import org.c4c.mysql.entity.User;
+import org.c4c.mysql.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getUsersByTenantId(String tenantId) {
+        return userRepository.findAllByTenantId(tenantId);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(updatedUser.getUsername());
+            user.setPassword(updatedUser.getPassword());
+            user.setEmail(updatedUser.getEmail());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
